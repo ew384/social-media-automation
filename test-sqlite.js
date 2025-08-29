a3ef1e74dc1dacfd6a0263d17ca7643e3de2b3f6
Endian Wang <ew384@cornell.edu>
2025-08-29 16:15:44 +0800

    init

--- test-sqlite.js
+++
@@ -1,7 +0,0 @@
-console.log('Testing better-sqlite3...');
-try {
-  const db = require('better-sqlite3');
-  console.log('better-sqlite3 loaded successfully');
-} catch(e) {
-  console.error('Error:', e.message);
-}
